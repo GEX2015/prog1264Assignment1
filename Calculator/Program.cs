55409df3558b4f974589aee087d0a599900455bf
Ricky Bastarache <Ricky Bastarache>
2016-09-30 13:51:09 -0300

    initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter left operand");
+            string input = Console.ReadLine();
+            double d;
+            if (!Double.TryParse(input, out d))
+                Console.WriteLine("Wrong input");
+            Console.WriteLine();
+
+            Console.WriteLine("enter left operand");
+        }
+    }
+}
