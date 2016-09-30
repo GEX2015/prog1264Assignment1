c6f5a63a45da4e12d8371d83e1bfc0ff27563514
chris arsenault <chris arsenault>
2016-09-30 13:46:56 -0300

    commit

---
+++ TvHRace/Tortoise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvHRace
+{
+    class Tortoise : Racer
+    {
+
+        public override void move() {
+            Random rnd = new Random();
+            int roll = rnd.Next(1, 11);
+            if (roll <= 5)
+                Position += 3;
+            else if (roll > 5 && roll <= 7)
+                Position -= 6;
+            else
+                Position++;
+
+        }
+        public override void draw() { Console.Write("Tortoise: "); base.draw(); Console.WriteLine(); }
+       
+    }
+}
