db2297f34a19b2120478be0ade439edb30fd1b5b
Robert Carll <Robert Carll>
2016-09-30 13:54:33 -0300

    initial commit

---
+++ Assignment1/Calculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Calculator
+    {
+        public string leftOperand = "";
+        public string rightOperand = "";
+        public string operand = "";
+        public double lhs = 0;
+        public double rhs = 0;
+
+        public void getOperands()
+        {
+            bool isNumber = false;
+            do
+            {
+                Console.WriteLine("Input your left number: ");              // Add validation
+                leftOperand = Console.ReadLine();
+                isNumber = Double.TryParse(leftOperand, out lhs);
+            } while (!isNumber);
+
+
+            Console.WriteLine("Input your operand: ");                  // add validation
+            operand = Console.ReadLine();
+
+            if (operand != "F" && operand != "f" && operand != "!")
+            {
+                Console.WriteLine("Input your right number: ");         // add validation
+                rightOperand = Console.ReadLine();
+                isNumber = Double.TryParse(rightOperand, out rhs);
+            }
+
+        }
+
+
+
+        public double add()
+        {
+            return lhs + rhs;
+        }
+        public double subtract()
+        {
+            return lhs - rhs;
+        }
+        public double multiply()
+        {
+            return lhs * rhs;
+        }
+        public double divide()
+        {
+            return lhs / rhs;
+        }
+
+    }
+}
---
+++ Assignment1/Program.cs
@@ -0,0 +1,52 @@
+﻿/**
+@file
+@author  Chris Arsenault Chris.arsenault06@gmail.com
+@version 1.0
+
+@section LICENSE
+
+This software is based on the material accompanying the book "SFML Game Development" see License.txt
+
+Additions and modifications are my sole work for prog 1266
+
+@section DESCRIPTION
+
+<your description of the purpose of this file here>
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Assignment1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            string quit = "";
+
+            do
+            {
+                
+
+                
+
+                
+
+
+
+                
+
+               
+
+
+            } while (quit != "q");
+        }
+    }
+}
